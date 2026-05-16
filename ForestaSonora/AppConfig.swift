319186cd4f9c0e32af8971951cb47e8585007221
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-16 20:36:44 +0300

    Update AppConfig.swift

--- ForestaSonora/AppConfig.swift
+++ ForestaSonora/AppConfig.swift
@@ -3,7 +3,7 @@ import Foundation
 enum AppConfig {
     static let canopyAnchor = URL(string: "https://djmorzat.com/1j7SY9")!
     static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/eac7b698-87fe-4452-93d1-19e3c0bc4faa")!
-    static let supportEmail = "rentrinas@icloud.com"
+    static let supportEmail = "jelegafa@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
